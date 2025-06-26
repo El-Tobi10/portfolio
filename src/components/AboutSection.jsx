854d0279e83b8d66b00f5bab38141c38217685a5
Tobias <tobiasalfonso@outlook.com>
2025-06-26 19:37:35 -0300

    .

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -50,7 +50,7 @@ export const AboutSection = () => {
                         </a>
 
                         <a
-                            href="public/proyectos/TobiasAlfonsoCV.pdf"
+                            href="/TobiasAlfonsoCV.pdf"
                             className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
                             download
                         >
